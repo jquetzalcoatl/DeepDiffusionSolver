48499f7a621a28e01be8c94fc4a4fcf3048a633f
Juliano <julianoferrarigianlupi@gmail.com>
2021-10-21 10:52:16 -0400

    fixing __init__

--- util/__init__.py
+++ util/__init__.py
@@ -1,4 +1,5 @@
 import os
-files = [f for f in os.listdir(os.currdir) 
-         if os.path.isfile(f) and f.endswith(*.py) and not f == '__init__.py']
+p = os.path.dirname((os.path.abspath(__file__)))
+files = [f for f in os.listdir(p) 
+         if os.path.isfile(f) and f.endswith('.py') and not f == '__init__.py']
 __all__ = files
