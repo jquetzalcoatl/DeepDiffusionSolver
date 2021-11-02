e4b8bad1ba199814611764a5f403e4ee14c87e1a
JulianoGianlupi <julianoferrarigianlupi@gmail.com>
2021-11-02 13:29:18 -0400

    added __all__

--- util/__init__.py
+++ util/__init__.py
@@ -5,3 +5,6 @@ import plotter
 import tools
 from fetch_dataset_name import fetch_dataset_name, get_Nplus_Nminus, get_number_sources_from_dataset_name
 from RelativeErrorLoss import RelativeErrorLoss
+
+__all__ = [RadialError, loaders, NNets, plotter, tools, fetch_dataset_name, get_Nplus_Nminus,
+           get_number_sources_from_dataset_name, RelativeErrorLoss]
