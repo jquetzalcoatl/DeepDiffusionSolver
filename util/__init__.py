a4de97217d51d3bf7659b42fea2513cf31fc60ab
JulianoGianlupi <julianoferrarigianlupi@gmail.com>
2021-11-05 10:26:25 -0400

    created custom losses package, moved relative error loss

--- util/__init__.py
+++ util/__init__.py
@@ -4,7 +4,7 @@ from . import NNets
 from . import plotter
 from . import tools
 from .fetch_dataset_name import fetch_dataset_name, get_Nplus_Nminus, get_number_sources_from_dataset_name
-from .RelativeErrorLoss import RelativeErrorLoss
+# from CustomLosses.RelativeErrorLoss import RelativeErrorLoss
 
 __all__ = [loaders, NNets, plotter, tools, fetch_dataset_name, get_Nplus_Nminus,
-           get_number_sources_from_dataset_name, RelativeErrorLoss]
+           get_number_sources_from_dataset_name]
